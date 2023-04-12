1ad7d75009c3da050967e376fc63849167d583c7
silviosalza <silviofilipposalza@gmail.com>
2023-04-12 14:51:23 +0200

    milestone 1 & bonus

---
+++ src/components/AppHero.vue
@@ -0,0 +1,19 @@
+<script>
+export default{
+    name:"AppHero",
+    data(){
+        return{
+            
+        }
+    }
+    
+}
+</script>
+
+<template>
+    <img src="../assets/tys6oq2smz091.png" alt="">
+</template>
+
+<style>
+
+</style>
---
+++ src/components/AppTitle.vue
@@ -0,0 +1,20 @@
+<script>
+export default{
+    name:"AppTitle",
+    data(){
+        return{
+            text: "La mia prima App con Vite"
+        }
+    }
+    
+}
+</script>
+
+<template>
+    <h1>{{ text }}</h1>
+  
+</template>
+
+<style>
+
+</style>
